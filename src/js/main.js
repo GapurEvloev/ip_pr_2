d0375fe86626a4eb58b9b924e8f4a1a1911d3ece
gapur <kaloycant@gmail.com>
2022-09-06 03:13:48 +0200

    #22 fin - burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ import calc from "./modules/calc";
 import filter from "./modules/filter";
 import pictureSize from "./modules/pictureSize";
 import accordion from "./modules/accordion";
+import burger from "./modules/burger";
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -18,12 +19,13 @@ window.addEventListener("DOMContentLoaded", () => {
   sliders(".main-slider-item", "vertical", ".main-prev-btn", ".main-next-btn");
   forms();
   mask("[name='phone']");
-  checkTextInputs('[name="name"]');
-  checkTextInputs('[name="message"]');
+  checkTextInputs("[name='name']");
+  checkTextInputs("[name='message']");
   showMoreStyles(".button-styles", ".styles-2");
   showMoreFetch(".button-styles", ".styles .row");
   calc("#size", "#material", "#options", ".promocode", ".calc-price");
   filter();
   pictureSize(".sizes-block");
   accordion(".accordion-heading", ".accordion-block");
+  burger(".burger-menu", ".burger");
 });
